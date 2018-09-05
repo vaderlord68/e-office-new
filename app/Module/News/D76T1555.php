7c32d9b0f0f6ed475b9036f5f34f9c9749696bd7
kimlong <kimlong@10.0.0.15>
2018-09-05 16:39:51 +0700

    Cập nhật code MH Danh mục chung

--- app/Module/News/D76T1555.php
+++ app/Module/News/D76T1555.php
@@ -4,21 +4,20 @@ namespace App\Module\News;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
-
-class D76T1556 extends \Eloquent
+class D76T1555 extends \Eloquent
 {
     /**
-     * Table name = D76T2140
+     * Table name = D76T1555
      * Table description: Module News: Entity News
      */
-    protected $table = 'D76T1556';
-    protected $primaryKey = 'ID';
+
+    protected $table = 'D76T1555';
+    protected $primaryKey = 'ListTypeID';
     protected $connection = 'sqlsrv';
     public $timestamps = false;
-    public function getList($listTypeID)
+    public function getList()
     {
-        $result = DB::table($this->table)->where('ListTypeID', $listTypeID)
-            ->get();
+        $result = DB::table($this->table)->get();
         return $result;
     }
 
---
+++ storage/framework/views/9f4a06d6d2c54e72c24722df364f5928ae120364.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+    <?php $__env->startSection('head'); ?>
+        <?php echo $__env->make('page.head', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
+    <?php echo $__env->yieldSection(); ?>
+</head>
+<?php $__env->startSection('body'); ?>
+    <body>
+    <?php $__env->startSection("left"); ?>
+        <?php echo $__env->make("page.left_panel", array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
+    <?php echo $__env->yieldSection(); ?>
+    <?php $__env->startSection("right"); ?>
+        <?php echo $__env->make("page.right_panel", array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
+    <?php echo $__env->yieldSection(); ?>
+    <?php echo $__env->make('page.footer_custom', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
+    <?php echo $__env->yieldSection(); ?>
+    <?php $__env->startSection('footer'); ?>
+    <?php echo $__env->yieldSection(); ?>
+    </body>
+<?php echo $__env->yieldSection(); ?>
+<div id="divModalContainer"></div>
+</html>
