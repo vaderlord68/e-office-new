2deb9c057dfac80f1ba4123aea20daeaf7f6abc5
DINH THI THANH TRAM <thanhtram.dinh@diginet.com.vn>
2018-09-07 09:31:38 +0700

    tram commit code

--- storage/framework/views/a0f56c0b885c7357affcbd7df040302777a56a3d.php
+++ storage/framework/views/a0f56c0b885c7357affcbd7df040302777a56a3d.php
@@ -1,8 +1,7 @@
-@extends('system.module.W76.W76F2142.components.layout')
-@section('news-wrapper')
-    <label class="labelb">{{Helpers::getRS("Tin_tuc_noi_bo")}}</label>
+<?php $__env->startSection('news-wrapper'); ?>
+    <label class="labelb"><?php echo e(Helpers::getRS("Tin_tuc_noi_bo")); ?></label>
     <div class="well">
-        @foreach($newsList as $newsRow)
+        <?php $__currentLoopData = $newsList; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $newsRow): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
             <?php
             $newsID = $newsRow->NewsID;
             $detailURL = url('/w76f2142/channel/?task=detail') . '&newsID=' . $newsID;
@@ -11,39 +10,45 @@
                 <div class="col-xs-5 col-sm-5 col-md-5 col-lg-5">
                     <div class="row mgb5">
                         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                            <a href="{{$detailURL}}"><img src="{{$newsRow->Image or ''}}"/></a>
+                            <a href="<?php echo e($detailURL); ?>"><img src="<?php echo e(isset($newsRow->Image) ? $newsRow->Image : ''); ?>"/></a>
                         </div>
                     </div>
                 </div>
                 <div class="col-xs-7 col-sm-7 col-md-7 col-lg-7 pdl10">
                     <div class="row font">
                         <div class="col-xs-12">
-                            <a href="{{$detailURL}}" class="nav-link cut-title">
-                                {{$newsRow->Title or ''}}
+                            <a href="<?php echo e($detailURL); ?>" class="nav-link cut-title">
+                                <?php echo e(isset($newsRow->Title) ? $newsRow->Title : ''); ?>
+
                             </a>
                         </div>
                     </div>
                     <div class="row mgl5">
                         <div class="col-xs-2 pdl10 ">
                             <a><i class="fa fa-eye left"></i>
-                                {{$newsRow-> ViewCount or ''}}
-                            </a>{{Helpers::getRS("Luot_xem")}}
+                                <?php echo e(isset($newsRow-> ViewCount) ? $newsRow-> ViewCount : ''); ?>
+
+                            </a><?php echo e(Helpers::getRS("Luot_xem")); ?>
+
                         </div>
                         <div class="col-xs-2 pdl10">
                             <a><i class="fa fa-calendar left"></i>
-                                {{$newsRow-> ReleaseDate or ''}}
+                                <?php echo e(isset($newsRow-> ReleaseDate) ? $newsRow-> ReleaseDate : ''); ?>
+
                             </a>
                         </div>
                     </div>
                     <div class="row mgl5">
                         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 cut-text">
                             <a>
-                                {!! $newsRow-> Content or '' !!}
+                                <?php echo isset($newsRow-> Content) ? $newsRow-> Content : ''; ?>
+
                             </a>
                         </div>
                     </div>
                 </div>
             </div>
-        @endforeach
+        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
     </div>
-@stop
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('system.module.W76.W76F2142.components.layout', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
