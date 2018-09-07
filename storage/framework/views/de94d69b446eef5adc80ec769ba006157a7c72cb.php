8db3b0e78163fb7657dbfc22dfc99df5592e30f8
Huynh Khanh <huynhkhanh@diginet.com.vn>
2018-09-07 09:22:26 +0700

    Khanh cap nhat css

---
+++ storage/framework/views/de94d69b446eef5adc80ec769ba006157a7c72cb.php
@@ -0,0 +1,14 @@
+<?php $__env->startSection('body_content'); ?>
+    ##parent-placeholder-5a4526adfe28f01223dccf37a363ace9165900d0##
+    <div class="rơw">
+        <div class="col-sm-3">
+            <?php echo $__env->make('system.module.W76.W76F2150.components.Sidebar', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
+        </div>
+        <div class="col-sm-9">
+            <?php echo $__env->make('system.module.W76.W76F2150.components.Wrapper', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
+        </div>
+
+    </div>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('page.master', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
