c5544b4f8696205fb169a1d66d0814e05a4b1fef
DINH THI THANH TRAM <thanhtram.dinh@diginet.com.vn>
2018-09-11 16:42:45 +0700

    tram commit code

--- storage/framework/views/65cb97a8ff9de1f797015d4d375995aeb62bf28b.php
+++ storage/framework/views/65cb97a8ff9de1f797015d4d375995aeb62bf28b.php
@@ -3,10 +3,10 @@
         <div class="col-sm-10 pdl5 pdr5">
             <div class="inline">
                 <a id="menuToggle" class="menutoggle pull-left"><i class="fa fa-angle-left"></i></a>
-                <a href="{{url('/')}}"><img class="logo" src="{{asset('media/logo-eOffice.png')}}" alt=""></a>
+                <a href="<?php echo e(url('/')); ?>"><img class="logo" src="<?php echo e(asset('media/logo-eOffice.png')); ?>" alt=""></a>
                 <div class="header-left right-menu collapse navbar-collapse" id="right-menu">
                     <style>
-                        @media screen and (min-width: 576px) {
+                        @media  screen and (min-width: 576px) {
                             .right-panel header.header div.header-left {
                                 display: block;
                             }
@@ -68,7 +68,7 @@
                                 <p>Manager Self Service</p>
                             </a>
                             <a class="dropdown-item media" href="/w76f2140">
-                                <p>{{Helpers::getRS('Quan_ly_ban_tin')}}</p>
+                                <p><?php echo e(Helpers::getRS('Quan_ly_ban_tin')); ?></p>
                             </a>
                         </div>
                     </div>
@@ -78,11 +78,11 @@
                             <span>Hệ thống</span>
                         </button>
                         <div class="dropdown-menu" aria-labelledby="notification">
-                            <a class="dropdown-item" href="{{url('/w76f1555')}}">
+                            <a class="dropdown-item" href="<?php echo e(url('/w76f1555')); ?>">
                                 <p>Danh mục dùng chung</p>
                             </a>
                             <a class="dropdown-item media" href="/w76f2200">
-                                <p>{{Helpers::getRS('Danh_sach_phong_hop')}}</p>
+                                <p><?php echo e(Helpers::getRS('Danh_sach_phong_hop')); ?></p>
                             </a>
 
                         </div>
@@ -95,7 +95,7 @@
         <div class="col-sm-2">
             <div class="user-area dropdown float-right">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <img class="user-avatar rounded-circle" src="{{ asset('media/default_user_icon.png') }}"
+                    <img class="user-avatar rounded-circle" src="<?php echo e(asset('media/default_user_icon.png')); ?>"
                          alt="User Avatar">
                 </a>
 
